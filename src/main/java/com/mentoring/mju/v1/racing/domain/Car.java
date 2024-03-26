0e27d32be3df16a72a804237d63a67f0dce8d5d7
승엽 <tmdduqflfl@naver.com>
2024-03-26 20:09:40 +0900

    n대의 자동차 입력 메서드 자동차 이름을 쉼표로 구분하게한다. 랜덤 숫자를 뽑느다. 4이상일시 전진하기 구현

--- src/main/java/com/mentoring/mju/v1/racing/domain/Car.java
+++ src/main/java/com/mentoring/mju/v1/racing/domain/Car.java
@@ -1,4 +1,40 @@
 package com.mentoring.mju.v1.racing.domain;
 
+import java.util.Random;
+
 public class Car {
+    private String carName;
+    private int number;
+    private final int BoundaryNumber = 10;
+    private int moveCount;
+    public Car(String carName) {
+        this.carName = carName;
+    }
+
+    public void Moving(int moveCount){
+        this.moveCount=getMoveCount();
+    }
+    public void Random(int number){
+        this.number=getNumber();
+    }
+
+    public String getCarName() {
+        carName.split(",");
+        return carName;
+    }
+
+    public int getNumber() {
+        //랜덤 숫자 데이터 받기
+        Random random = new Random();
+        random.nextInt(BoundaryNumber);
+        return number;
+    }
+
+    public int getMoveCount() {
+        // 숫자가 만약 4이상이면 moveCount++
+        if (getNumber() >= 4) {
+            moveCount++;
+        }
+        return moveCount;
+    }
 }
