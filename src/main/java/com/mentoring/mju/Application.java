14f0cc5e39c02040c00fcfef9d50acbf5ca76723
승엽 <tmdduqflfl@naver.com>
2024-04-02 20:07:25 +0900

    수정중

--- src/main/java/com/mentoring/mju/Application.java
+++ src/main/java/com/mentoring/mju/Application.java
@@ -1,13 +1,16 @@
 package com.mentoring.mju;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import com.mentoring.mju.v1.racing.controller.RacingController;
+
+
 public class Application {
-
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        RacingController racingController = new RacingController();
+        racingController.startRacing();
     }
-
 }
--- src/test/java/com/mentoring/mju/ApplicationTests.java
+++ src/test/java/com/mentoring/mju/ApplicationTests.java
@@ -1,5 +1,7 @@
 package com.mentoring.mju;
 
+import com.mentoring.mju.v1.racing.controller.RacingController;
+import com.mentoring.mju.v1.racing.domain.Car;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,9 @@ class ApplicationTests {
 
     @Test
     void contextLoads() {
+        RacingController racingController =new RacingController();
+        racingController.startRacing();
+
     }
 
 }
