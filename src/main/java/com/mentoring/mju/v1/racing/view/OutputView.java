db2cfc742ad1e1efa397c45b5690b9be6f7cfad0
승엽 <tmdduqflfl@naver.com>
2024-04-09 14:08:43 +0900

    메서드 분리

--- src/main/java/com/mentoring/mju/v1/racing/view/OutputView.java
+++ src/main/java/com/mentoring/mju/v1/racing/view/OutputView.java
@@ -8,12 +8,18 @@ public class OutputView {
     public void printResult(List<Car> cars) {
         for (Car car : cars) {
             String result = car.getCarName() + ": ";
-            for (int i = 0; i < car.getMoveCount(); i++) {
-                result += "-";
-            }
+            result = ConversionMove(car, result);
             System.out.println(result);
         }
     }
+
+    private static String ConversionMove(Car car, String result) {
+        for (int i = 0; i < car.getMoveCount(); i++) {
+            result += "-";
+        }
+        return result;
+    }
+
     public void printWinners(List<Car> winners) {
         System.out.println("Winner : ");
         for (Car winner : winners) {
